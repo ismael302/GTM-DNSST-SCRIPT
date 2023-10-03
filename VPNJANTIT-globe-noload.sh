22067e5bc3604a790b87df903eec30acfa56baf9
ismael302 <146759270+ismael302@users.noreply.github.com>
2023-10-03 11:33:57 +0800

    Update VPNJANTIT-globe-noload.sh

--- VPNJANTIT-globe-noload.sh
+++ VPNJANTIT-globe-noload.sh
@@ -5,8 +5,8 @@
 ## run command: ./globe-civ3.sh l
 
 ## Your DNSTT Nameserver & your Domain `A` Record
-NS='b2.dns.slowdns.online'
-A='b1.dns.slowdns.online'
+NS='ns-ph3.vpnjantit.com'
+A='ph3.vpnjantit.com'
 ## Repeat dig cmd loop time (seconds) (positive interger only)
 LOOP_DELAY=5
 
